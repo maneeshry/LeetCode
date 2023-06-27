3516f09570fed2894344c17040dce716abfe82b7
maneeshry <yalamareddymaneesh@gmail.com>
2023-06-26 23:06:52 -0500

    complete web Scrapper app

--- Scrapper/Scrapper/src/WebScrape.java
+++ Scrapper/Scrapper/src/WebScrape.java
@@ -1,49 +1,54 @@
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-
-/**
- *
- * @author Shane - shanelee.co.uk
-*/
-
-
 public class WebScrape {
     public static void main(String[] args) {
-        final String url = 
-"https://www.booking.com/searchresults.html?ss=Biloxi&ssne=Biloxi&ssne_untouched=Biloxi&label=gen173nr-1FCAEoggI46AdIM1gEaJ4CiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAL7ufKjBsACAdICJDM3ZmRlYjJhLWU1ZGUtNDBhOS1iMzE3LTcwZTFkZTAzYWYzZNgCBeACAQ&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=20068916&dest_type=city&checkin=2023-06-23&checkout=2023-06-24&group_adults=2&no_rooms=1&group_children=0"   ;     
+
+        Scanner input = new Scanner(System.in);
+        final String checkin = LocalDate.now().toString();
+        final String checkout = LocalDate.now().plusDays(1).toString();
+
+        System.out.println("Enter the number of adults: ");
+        final int group_adults = input.nextInt();
+
+        System.out.println("Enter the number of rooms: ");
+        final int no_rooms = input.nextInt();
+        
+        System.out.println("Enter the number of children: ");
+        final int group_children = input.nextInt();
+        input.close();
+
+        final String url = "https://www.booking.com/searchresults.html?ss=Biloxi&ssne=Biloxi&ssne_untouched=Biloxi&label=gen173nr-1FCAEoggI46AdIM1gEaJ4CiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAL7ufKjBsACAdICJDM3ZmRlYjJhLWU1ZGUtNDBhOS1iMzE3LTcwZTFkZTAzYWYzZNgCBeACAQ&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=20068916&dest_type=city&checkin="
+                + checkin + "&checkout=" + checkout + "&group_adults=" + group_adults + "&no_rooms=" + no_rooms
+                + "&group_children=" + group_children;
+       
         try {
             final Document document = Jsoup.connect(url).get();
-           
+
             for (Element row : document.select(
-                "div.a1b3f50dcd.be36d14cea.f7c6687c3d.f996d8c258")) { //f996d8c258.f7c6687c3d.be36d14cea.a1b3f50dcd
-                    
+                    "div.a1b3f50dcd.be36d14cea.f7c6687c3d.f996d8c258")) { // f996d8c258.f7c6687c3d.be36d14cea.a1b3f50dcd
+
                 if (!row.select("div.bui-review-score__badge").isEmpty()) {
-                    
+
                     continue;
-                }
-                else {
+                } else {
                     final String name = row.select(
-                            "div.fcab3ed991.a23c043802").text(); //span.fcab3ed991.fbd1d3018c.e729ed5ab6
-                    
-System.err.println(name);
-                    //get the price of the hotel
+                            "div.fcab3ed991.a23c043802").text(); // span.fcab3ed991.fbd1d3018c.e729ed5ab6
 
                     final String price = row.select("span.fcab3ed991.fbd1d3018c.e729ed5ab6").text();
-                            System.err.println(price);
-
-                    
-
-
+                    System.err.println(name + " --- " + price);
+                    System.out.println();
                 }
             }
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-        
+
     }
-    
+
 }
--- WebsiteTracker.java
+++ WebsiteTracker.java
@@ -1,29 +1,45 @@
-import java.math.BigInteger;
-import java.net.URL;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
-public class WebsiteTracker {
+/**
+ *
+ * @author Shane - shanelee.co.uk
+*/
 
-  public static String getHashOfUrl(byte[] bytes) throws NoSuchAlgorithmException {
-    if( bytes == null) return "";
-    MessageDigest md = MessageDigest.getInstance("SHA-1");
-    md.update(bytes);
-    byte[] digest = md.digest();
-    
-    // convert it to the hexadecimal 
-    BigInteger bi = new BigInteger(digest);
-    return bi.toString(16);
-}
-  public static void main(String[] args) {
-    String simplehash = null;
-    try {
-      simplehash = getHashOfUrl("https://itachi-uchiha-013.000webhostapp.com/".getBytes());
-    } catch (NoSuchAlgorithmException e) {
-      
-      e.printStackTrace();
+
+public class WebScrape {
+    public static void main(String[] args) {
+        final String url = 
+                "https://sharestobeclosed.telegraph.co.uk/indices/financials/index/MCX";
+        
+        try {
+            final Document document = Jsoup.connect(url).get();
+            
+            for (Element row : document.select(
+                "table.tablesorter.full tr")) {
+                if (row.select("td:nth-of-type(1)").text().equals("")) {
+                    continue;
+                }
+                else {
+                   final String ticker = 
+                           row.select("td:nth-of-type(1)").text();
+                   final String name = 
+                           row.select("td:nth-of-type(2)").text();
+                   final String tempPrice = 
+                           row.select("td.right:nth-of-type(3)").text();
+                   final String tempPrice1 = 
+                           tempPrice.replace(",", "");
+//                    final double price = Double.parseDouble(tempPrice1);
+                   
+                    System.out.println(ticker + " " + name + " " + tempPrice1);
+                }
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
     }
-    System.out.println(simplehash);
-  }
-  
+    
 }
